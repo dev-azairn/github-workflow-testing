45a52a4bb17e5a61b92147986624b29e9340bf68
Dev-Azairn <rain-piyapong@outlook.com>
2026-07-01 15:05:01 +0700

    edit wrong test case

--- tests/test_player.gd
+++ tests/test_player.gd
@@ -1,11 +1,11 @@
 extends GutTest
 
 func test_take_damage_reduces_health():
-	var player = load("res://scripts/Player.gd").new()
+	var player = load("res://Scripts/Player.gd").new()
 	player.take_damage(30)
 	assert_eq(player.health, 70, "Health should be reduced by 30")
 
 func test_health_does_not_go_negative():
-	var player = load("res://scripts/Player.gd").new()
+	var player = load("res://Scripts/Player.gd").new()
 	player.take_damage(200)
 	assert_eq(player.health, 0, "Health should not go below 0")
